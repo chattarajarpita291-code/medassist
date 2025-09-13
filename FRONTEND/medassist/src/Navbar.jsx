ec3190465868ecc0117106618d4eae7b35e09e41
tanaya242004 <samirbrahma2003@gmail.com>
2025-09-13 15:44:12 +0530

    backend-frontend chages

--- FRONTEND/medassist/src/Navbar.jsx
+++ FRONTEND/medassist/src/Navbar.jsx
@@ -1,9 +1,41 @@
-// Navbar.jsx
-import React from 'react';
+// // Navbar.jsx
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import "./assets/css/Navbar.css";
+// import bgimage from './assets/images/ChatGPT_Image_Jul_31__2025__03_56_15_PM-removebg-preview.png';
+// const Navbar = () => {
+//     return (
+//         <div className='navbar'>
+//             <div className="box1">
+//                 <div className="sbox1">
+//                     <img src={bgimage} alt="bg" />
+//                     <div className="sbox2">MediAssist+</div>
+//                 </div>
+//             </div>
+//             <div className="box2">
+//                 <ul className="nav-list">
+//                     <li><Link to="/">Home</Link></li>
+//                     <li><Link to="/about">About Us</Link></li>
+//                     <li><Link to="/service">Services</Link></li>
+//                     <li><Link to="/doctors">Doctors</Link></li>
+//                     <li><Link to="/department">Departments</Link></li>
+//                 </ul>
+//             </div>
+//             <a href="#" className="btn-nav">MediBot<span><i></i></span></a>
+//         </div>
+//     );
+// };
+
+// export default Navbar;
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./assets/css/Navbar.css";
 import bgimage from './assets/images/ChatGPT_Image_Jul_31__2025__03_56_15_PM-removebg-preview.png';
+
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <div className='navbar'>
             <div className="box1">
@@ -13,7 +45,15 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="box2">
-                <ul className="nav-list">
+                <div
+                    className={`hamburger ${menuOpen ? "open" : ""}`}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    <span />
+                    <span />
+                    <span />
+                </div>
+                <ul className={`nav-list ${menuOpen ? "show" : ""}`}>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About Us</Link></li>
                     <li><Link to="/service">Services</Link></li>
